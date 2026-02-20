b7016b7812e1997cb33977ebb18aa3b1864a4929
Muhammad Yasir <my3154831409@gmail.com>
2026-02-20 22:24:33 +0500

    feat(sound): add custom sound, success melody and cleanup functionality

--- src/utils/soundEffects.js
+++ src/utils/soundEffects.js
@@ -142,6 +142,107 @@ class SoundManager {
     this.play('complete');
   }
 
- 
+  /**
+   * Play test start sound
+   */
+  playStart() {
+    this.play('start');
+  }
+
+  /**
+   * Play custom sound with specific frequency and volume
+   * @param {number} frequency - Sound frequency in Hz
+   * @param {number} volume - Volume (0 to 1)
+   * @param {number} duration - Duration in seconds
+   */
+  playCustom(frequency = 440, volume = 0.1, duration = 0.08) {
+    if (!this.isEnabled || !this.audioContext) {
+      return;
+    }
+
+    const oscillator = this.audioContext.createOscillator();
+    const gainNode = this.audioContext.createGain();
+
+    oscillator.connect(gainNode);
+    gainNode.connect(this.audioContext.destination);
+
+    oscillator.frequency.value = frequency;
+    gainNode.gain.setValueAtTime(0, this.audioContext.currentTime);
+    gainNode.gain.linearRampToValueAtTime(
+      volume,
+      this.audioContext.currentTime + 0.01
+    );
+    gainNode.gain.linearRampToValueAtTime(
+      0,
+      this.audioContext.currentTime + duration
+    );
+
+    oscillator.start(this.audioContext.currentTime);
+    oscillator.stop(this.audioContext.currentTime + duration);
+  }
+
+  /**
+   * Play success melody (for achievements)
+   */
+  playSuccess() {
+    if (!this.isEnabled || !this.audioContext) {
+      return;
+    }
+
+    const notes = [
+      { frequency: 523.25, duration: 0.1 }, // C5
+      { frequency: 659.25, duration: 0.1 }, // E5
+      { frequency: 783.99, duration: 0.2 }  // G5
+    ];
+
+    let time = this.audioContext.currentTime;
+
+    notes.forEach((note) => {
+      const oscillator = this.audioContext.createOscillator();
+      const gainNode = this.audioContext.createGain();
+
+      oscillator.connect(gainNode);
+      gainNode.connect(this.audioContext.destination);
+
+      oscillator.frequency.value = note.frequency;
+      gainNode.gain.setValueAtTime(0, time);
+      gainNode.gain.linearRampToValueAtTime(0.15, time + 0.01);
+      gainNode.gain.linearRampToValueAtTime(0, time + note.duration);
+
+      oscillator.start(time);
+      oscillator.stop(time + note.duration);
+
+      time += note.duration;
+    });
+  }
+
+  /**
+   * Clean up audio context
+   */
+  cleanup() {
+    if (this.audioContext) {
+      this.audioContext.close();
+      this.audioContext = null;
+    }
+  }
+}
+
+// Create singleton instance
+const soundManager = new SoundManager();
+
+// Export individual functions
+export const initSound = () => soundManager.init();
+export const enableSound = () => soundManager.enable();
+export const disableSound = () => soundManager.disable();
+export const toggleSound = () => soundManager.toggle();
+export const playCorrectSound = () => soundManager.playCorrect();
+export const playErrorSound = () => soundManager.playError();
+export const playCompleteSound = () => soundManager.playComplete();
+export const playStartSound = () => soundManager.playStart();
+export const playCustomSound = (frequency, volume, duration) => 
+  soundManager.playCustom(frequency, volume, duration);
+export const playSuccessSound = () => soundManager.playSuccess();
+export const cleanupSound = () => soundManager.cleanup();
+
 // Export the manager instance
 export default soundManager;
