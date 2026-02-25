10431d9d013b12558065babcfd34dc387855652c
Muhammad Yasir <my3154831409@gmail.com>
2026-02-26 00:12:11 +0500

    Update HistoryContext.jsx with actual data

---
+++ src/context/HistoryContext.jsx
@@ -0,0 +1,36 @@
+// History Context Provider
+
+import React, { createContext, useContext } from 'react';
+import useHistory from '../hooks/useHistory';
+
+// Create History Context
+const HistoryContext = createContext(undefined);
+
+/**
+ * History Provider Component
+ * Provides history state and functions to all children
+ */
+export const HistoryProvider = ({ children }) => {
+  const historyState = useHistory();
+
+  return (
+    <HistoryContext.Provider value={historyState}>
+      {children}
+    </HistoryContext.Provider>
+  );
+};
+
+/**
+ * Custom hook to use History Context
+ * @returns {object} - History state and functions
+ */
+export const useHistoryContext = () => {
+  const context = useContext(HistoryContext);
+  
+  if (context === undefined) {
+    throw new Error('useHistoryContext must be used within a HistoryProvider');
+  }
+  
+  return context;
+};
+
