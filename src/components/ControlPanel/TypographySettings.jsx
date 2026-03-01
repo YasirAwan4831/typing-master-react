c93be8ba6648bb14acbd9cc89b87935acf2732eb
Muhammad Yasir <my3154831409@gmail.com>
2026-03-01 23:05:04 +0500

    Update TypographySettings component (85% complete)

--- src/components/ControlPanel/TypographySettings.jsx
+++ src/components/ControlPanel/TypographySettings.jsx
@@ -1,1 +1,46 @@
-// TypographySettings component (Work in progress)
+// Typography Settings Component
+
+
+/**
+ * Typography Settings Component
+ * Allows customization of font size and font family
+ */
+const TypographySettings = ({
+  fontSize,
+  fontFamily,
+  onFontSizeChange,
+  onFontFamilyChange
+}) => {
+  return (
+    <div className={styles.controlSection}>
+      <label className={styles.controlLabel}>Typography</label>
+      <div className={styles.controlOptions}>
+        {/* Font Size Selector */}
+        <select
+          className={styles.customSelect}
+          value={fontSize}
+          onChange={(e) => onFontSizeChange(e.target.value)}
+        >
+          <option value={FONT_SIZE_OPTIONS.SMALL}>Small</option>
+          <option value={FONT_SIZE_OPTIONS.NORMAL}>Normal</option>
+          <option value={FONT_SIZE_OPTIONS.LARGE}>Large</option>
+          <option value={FONT_SIZE_OPTIONS.EXTRA_LARGE}>Extra Large</option>
+        </select>
+
+        {/* Font Family Selector */}
+        <select
+          className={styles.customSelect}
+          value={fontFamily}
+          onChange={(e) => onFontFamilyChange(e.target.value)}
+        >
+          <option value={FONT_FAMILY_OPTIONS.JETBRAINS}>JetBrains Mono</option>
+          <option value={FONT_FAMILY_OPTIONS.COURIER}>Courier New</option>
+          <option value={FONT_FAMILY_OPTIONS.DM_SANS}>DM Sans</option>
+          <option value={FONT_FAMILY_OPTIONS.SYSTEM}>System Mono</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default TypographySettings;
