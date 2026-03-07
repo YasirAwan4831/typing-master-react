91f8a531443c2b6f8e58894b79fa29dc8e2311c2
Muhammad Yasir <my3154831409@gmail.com>
2026-03-07 18:03:31 +0500

    Update index.js Import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 // TODO: Add data later
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
